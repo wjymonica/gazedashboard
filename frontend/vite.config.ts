c5f6e00b775ffc33699cf5f9acdb048a58969088
Jingying Wang <jingyingwang@JingyingdeMacBook-Air-6.local>
2025-11-20 14:32:56 -0500

    vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: targetUrl,
+        target: 'http://localhost:5052',
         changeOrigin: true,
         secure: false,
       },
